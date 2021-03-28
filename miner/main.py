94b1c66529c00e5eefe8796b3acdf779a345222f
alexfayers <alex@fayers.com>
2021-03-28 21:29:51 +0000

    int

--- miner/main.py
+++ miner/main.py
@@ -385,6 +385,7 @@ try:
     with fs.open(BLOCK_LOG_FILENAME, "r") as f:
         for line in f:
             latest_branch = line
+            latest_branch = int(latest_branch)
             break
 except Exception:
     pass
