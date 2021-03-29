159300c207aa6fdedf1ae5a3a22c3fb06bcaa6f7
alexfayers <alex@fayers.com>
2021-03-29 20:23:39 +0000

    rejig

--- miner/main.py
+++ miner/main.py
@@ -1,14 +1,14 @@
 from cc import is_turtle, turtle
 from cc import import_file
 
-miner = import_file('miner.py', __file__)
-station = import_file('station.py', __file__)
 
 if not is_turtle():
     print("Running on a turtle - starting miner script!")
+    miner = import_file('miner.py', __file__)
     miner.mine()
 else:
     print("Running on a computer - starting station script!")
+    station = import_file('station.py', __file__)
     station.init()
 
 print("Python exit!")
