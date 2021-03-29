7511ea05e25736ba8fd3293a36a6ad09205c4138
alexfayers <alex@fayers.com>
2021-03-29 20:24:06 +0000

    logic

--- miner/main.py
+++ miner/main.py
@@ -2,7 +2,7 @@ from cc import is_turtle, turtle
 from cc import import_file
 
 
-if not is_turtle():
+if is_turtle():
     print("Running on a turtle - starting miner script!")
     miner = import_file('miner.py', __file__)
     miner.mine()
