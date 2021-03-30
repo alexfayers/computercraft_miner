af8242da06f7f7d632f46db4daa9f2ddeeafbec8
alexfayers <alex@fayers.com>
2021-03-30 14:27:31 +0000

    please

--- miner/main.py
+++ miner/main.py
@@ -6,12 +6,12 @@ print("Start!")
 if is_turtle():
     print("Running on a turtle - starting miner script!")
 
-    if 1 == 2 and input("quarry or strip? (q/s): ").lower() == "s":
-        miner = import_file("miner.py", __file__)
-        miner.mine()
-    else:
-        miner = import_file("quarry.py", __file__)
-        miner.mine()
+    #if 1 == 2 and input("quarry or strip? (q/s): ").lower() == "s":
+    #    miner = import_file("miner.py", __file__)
+    #    miner.mine()
+    #else:
+    miner = import_file("quarry.py", __file__)
+    miner.mine()
 else:
     print("Running on a computer - starting station script!")
     station = import_file("station.py", __file__)
