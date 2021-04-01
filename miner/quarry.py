df3af47dfb192207324f21647e6c9e3af7bbebe0
alexfayers <alex@fayers.com>
2021-04-01 23:00:36 +0000

    please

--- miner/quarry.py
+++ miner/quarry.py
@@ -307,7 +307,7 @@ def mine_layer():
 
 
 def mine_several_layers():
-    while END_Y < CURRENT_Y:
+    while END_Y <= CURRENT_Y:
         mine_layer()
 
         if CHUNK_SIZE % 2 == 0:
@@ -315,7 +315,7 @@ def mine_several_layers():
         else:
             turtle.turnLeft()
 
-        if END_Y + 1 < CURRENT_Y:
+        if END_Y < CURRENT_Y:
             down_layer()
 
         print(f"y={CURRENT_Y} complete")
