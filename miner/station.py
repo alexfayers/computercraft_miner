18e931a5e8b4ceb4e9ae160b36ab5c1be13e1124
alexfayers <alex@fayers.com>
2021-04-02 00:38:37 +0000

    parallel!

--- miner/station.py
+++ miner/station.py
@@ -1,11 +1,31 @@
 from cc import rednet
 from cc import peripheral
+from cc import parallel
 
 import requests
 
 JOIN_KEY = requests.get("http://192.168.1.54:8000/join.key").text
 
 
+def listen_for_response(modem_name):
+    while True:
+        response = rednet.receive("QuarryMiner"):
+        if message == None:
+            break
+        else:
+            for message in response:
+                print(repr(msg))
+
+
+def command_control(modem_name):
+    while True:
+        command = input("QuarryControl> ").encode()
+
+        if command == b"exit":
+            print("Bye")
+            break
+
+        rednet.broadcast(command, "QuarryControl")
 
 
 def init():
@@ -17,22 +37,12 @@ def init():
 
     if not rednet.isOpen(modem_name):
         rednet.open(modem_name)
+        print("Opening modem")
+    else:
+        print("Modem is open")
+    
+    parallel.waitForAny(command_control, listen_for_response)
 
-    while True:
-        command = input("QuarryControl> ").encode()
-
-        if command == b"exit":
-            print("Bye")
-            break
-
-        rednet.broadcast(command, "QuarryControl")
-
-        if command == b"ping":
-            response = rednet.receive("QuarryMiner", 5):
-                if message == None:
-                    break
-                else:
-                    for message in response:
-                        print(repr(msg))
+    print("Closing modem")
 
     rednet.close(modem_name)
