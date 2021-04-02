e180484254332bc27c203cdbcb78c652e543e2e9
alexfayers <alex@fayers.com>
2021-04-02 00:07:24 +0000

    init station

--- miner/station.py
+++ miner/station.py
@@ -1,7 +1,28 @@
+from cc import rednet
+from cc import peripheral
+
 import requests
 
 JOIN_KEY = requests.get("http://192.168.1.54:8000/join.key").text
 
 
 def init():
-    print("WIP.")
+    MODEM_SIDE = 'right'
+
+    modem = peripheral.wrap(side)
+    modem_name = modem.getName()
+
+    if not rednet.isOpen(modem_name):
+        rednet.open(modem_name)
+    
+    while True:
+        command = input("QuarryControl> ")
+
+        if command == 'exit':
+            print("Bye")
+            break
+
+        rednet.broadcast(command, "QuarryControl")
+
+    rednet.close(modem_name)
+`
